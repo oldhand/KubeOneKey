d9fac44b620e52ca3d24b0610a20acdb1eabf44c
oldhand <68594864@qq.com>
2025-09-24 03:23:24 +0800

    no message

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 获取操作系统名称
-os_name=$(cat /etc/os-release | grep NAME | head -n1 | cut -d= -f2 | sed 's/"//g')
+os_name=$(cat /etc/os-release | grep NAME | head -n1 | cut -d= -f2 | sed 's/"//g' | cut -d' ' -f1)
 
 # 获取CPU架构
 cpu_arch=$(uname -m)
