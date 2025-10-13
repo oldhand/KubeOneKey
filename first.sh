13965f5f525a2baf901fe1b67457689b639a3548
oldhand <68594864@qq.com>
2025-10-13 15:21:50 +0800

    no message

--- first.sh
+++ first.sh
@@ -3,24 +3,14 @@
 # 获取操作系统名称
 os_name=$(cat /etc/os-release | grep NAME | head -n1 | cut -d= -f2 | sed 's/"//g' | cut -d' ' -f1)
 
-# 根据操作系统类型获取版本号（含小版本）
-if [ "$os_name" = "CentOS" ] || [ "$os_name" = "RedHat" ] || [ "$os_name" = "Rocky" ] || [ "$os_name" = "AlmaLinux" ]; then
-    # RHEL系（CentOS/Rocky等）从专门的release文件提取版本
-    os_version=$(cat /etc/redhat-release | grep -oE '([0-9]+\.)+[0-9]+' | cut -d. -f1,2)
-else
-    # 其他发行版（Ubuntu/Debian等）从os-release的VERSION字段提取
-    os_version=$(cat /etc/os-release | grep '^VERSION=' | head -n1 | cut -d= -f2 | sed 's/"//g' | grep -oE '[0-9]+\.[0-9]+')
-fi
-
 # 获取CPU架构
 cpu_arch=$(uname -m)
 
 # 输出结果
 echo "操作系统: $os_name"
-echo "操作系统版本: $os_version"
 echo "CPU架构: $cpu_arch"
 
-## 仅在 openEuler 系统上禁用 Swap
+# 仅在 openEuler 系统上禁用 Swap
 if [ "$os_name" = "openEuler" ]; then
    rpm -ivh $(pwd)/packages/$os_name/$cpu_arch/*.rpm --force --nodeps
 elif [ "$os_name" = "CentOS" ]; then
@@ -29,10 +19,18 @@ elif [ "$os_name" = "Ubuntu" ]; then
    sudo dpkg -i ./packages/$os_name/$cpu_arch/*.deb
 fi
 
-ansible-galaxy collection install $(pwd)/kubernetes/kubernetes-core-6.1.0.tar.gz --force
+
 
 if [ "$os_name" = "CentOS" ]; then
-    pip3 install --no-index --find-links=$(pwd)/pip/$os_name/$os_version kubernetes PyYAML
+    rpm -e ansible-2.9.27-1.el7.noarch
+    pip3 install $(pwd)/pip/$os_name/pip-21.3.1-py3-none-any.whl
+    pip3 install --no-index --find-links=$(pwd)/pip/$os_name ansible
+    ln -s /usr/local/bin/ansible /usr/bin/ansible-playbook
+    ln -s /usr/local/bin/ansible /usr/bin/ansible-galaxy
+    ln -s /usr/local/bin/ansible /usr/bin/ansible
+    ansible-galaxy collection install $(pwd)/kubernetes/kubernetes-core-6.1.0.tar.gz --force
+    pip3 install --no-index --find-links=$(pwd)/pip/$os_name kubernetes PyYAML
 else
+  ansible-galaxy collection install $(pwd)/kubernetes/kubernetes-core-6.1.0.tar.gz --force
   pip3 install --no-index --find-links=$(pwd)/pip/$os_name kubernetes PyYAML
 fi
